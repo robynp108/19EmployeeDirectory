fa607345b5b3ba8a91a8cadf338326f77ebb20af
robynp108 <robynp108@gmail.com>
2020-05-12 19:26:30 -0500

    completed table to display all employees

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import employees from "./employees.json";
+import EmployeeCard from "./components/EmployeeCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    employees
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>Employee Directory</h2>
+        <div>
+          <table>
+            <tr>
+              <th>Name</th>
+              <th>Title</th>
+              <th>Department</th>
+            </tr>
+            {this.state.employees.map(employee => (
+              <EmployeeCard
+                name={employee.name}
+                title={employee.title}
+                department={employee.department}
+              />
+            ))}
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/EmployeeCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function EmployeeCard(props) {
+    return (
+        <tr>
+            <td>{props.name}</td>
+            <td>{props.title}</td>
+            <td>{props.department}</td>
+        </tr>
+    )
+}
+
+export default EmployeeCard;
